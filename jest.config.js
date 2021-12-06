108deb33b5f698837fd94b8f962fc525f2044b9b
Jaakko Pallari <jkpl@lepovirta.org>
2021-12-06 15:59:17 +0200

    JavaScript coverage reporting to SonarCloud

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,10 @@
 module.exports = {
+  collectCoverage: true,
   transform: {
     '\\.m?jsx?$': 'jest-esm-transformer',
   },
+  testMatch: ['**/*.test.js'],
+  coverageReporters: ['text', 'lcov', 'cobertura'],
+  reporters: ['default', 'jest-junit'],
+  testResultsProcessor: 'jest-sonar-reporter',
 };
